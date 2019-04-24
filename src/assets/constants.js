9539e8799397c2d69e85e001a7876a3025594694
Zhou <zydiceland@gmail.com>
2019-04-24 16:12:22 +0800

    配置ci

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -8,7 +8,7 @@ export const NETWORK = {
 }
 
 export const API_URL = {
-  API_POLL_TALLY: 'https://s3.amazonaws.com/api.eosvotes.io/eosvotes/tallies/latest.json',
-  API_GET_ALL_VOTES: 'https://s3.amazonaws.com/api.eosvotes.io/eosio.forum/vote/latest.json',
-  API_GET_ALL_VOTERS: 'https://s3.amazonaws.com/api.eosvotes.io/eosvotes/accounts/latest.json'
+  API_POLL_TALLY: 'https://s3.amazonaws.com/bostest.referendum/referendum/tallies/latest.json',
+  API_GET_ALL_VOTES: 'https://s3.amazonaws.com/bostest.referendum/bosforumapp/vote/latest.json',
+  API_GET_ALL_VOTERS: 'https://s3.amazonaws.com/bostest.referendum/eosio/voters/latest.json'
 }
