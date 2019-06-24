aa621c08e41e1cf66c1beab625811f3d5378ae86
Denis Carriere <carriere.denis@gmail.com>
2019-06-24 14:48:12 -0400

    Add validate JSON for `updatebio`

--- contracts/auditor.bos/src/update_member_details.cpp
+++ contracts/auditor.bos/src/update_member_details.cpp
@@ -1,6 +1,9 @@
+#include "validate_json.cpp"
 
 void auditorbos::updatebio(name cand, string bio) {
     require_auth(cand);
+    VALIDATE_JSON(bio, 32768);
+
     const auto &reg_candidate = registered_candidates.get(cand.value, "ERR::UPDATEBIO_NOT_CURRENT_REG_CANDIDATE::Candidate is not already registered.");
 
     auto cand_bio = candidate_bios.find(cand.value);
