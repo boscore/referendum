e5fa4dcd067fa9e9efdae5c980fb34915370f8cf
Denis Carriere <carriere.denis@gmail.com>
2019-06-26 11:16:35 -0400

    Zero existing votes (vote counting is done offchain via BOS Referendum Tally)

--- contracts/auditor.bos/src/voting.cpp
+++ contracts/auditor.bos/src/voting.cpp
@@ -7,20 +7,27 @@ void auditorbos::vote(name voter, vector<name> candidates) {
     std::set<name> dupSet{};
     for (name candidate_name: candidates) {
         check(dupSet.insert(candidate_name).second, "ERR::VOTE_DUPLICATE_VOTES::Added duplicate votes for the same candidate.");
-        auto candidate = _candidates.get(candidate_name.value, "ERR::VOTE_CANDIDATE_NOT_FOUND::Candidate could not be found.");
-        check(!candidate.is_active, "ERR::VOTE_VOTING_FOR_INACTIVE_CAND::Attempting to vote for an inactive candidate.");
+
+        auto candidate_itr = _candidates.find(candidate_name.value);
+        check(candidate_itr != _candidates.end(), "ERR::VOTE_CANDIDATE_NOT_FOUND::Candidate could not be found.");
+        check(candidate_itr->is_active, "ERR::VOTE_VOTING_FOR_INACTIVE_CAND::Attempting to vote for an inactive candidate.");
+
+        // Zero existing votes (vote counting is done offchain via BOS Referendum Tally)
+        _candidates.modify(candidate_itr, _self, [&]( auto& row) {
+            row.total_votes = 0;
+        });
     }
 
     const auto & voter_itr = _votes.find(voter.value);
 
     // Modify existing registered candidate with additional locked_tokens
     if (voter_itr != _votes.end()) {
-        _votes.modify(voter_itr, eosio::same_payer, [&](auto & row) {
+        _votes.modify(voter_itr, eosio::same_payer, [&](auto& row) {
             row.candidates = candidates;
         });
     // New candidate, register them and add locked_tokens
     } else {
-        _votes.emplace(_self, [&](auto & row) {
+        _votes.emplace(_self, [&](auto& row) {
             row.voter = voter;
             row.candidates = candidates;
         });
