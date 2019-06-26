85a1563a0072c0ededbfc51c263ea7fe161201ef
Denis Carriere <carriere.denis@gmail.com>
2019-06-26 11:36:45 -0400

    Auto nominate candidate if locked_tockens is above minimum threshold

--- contracts/auditor.bos/src/external_observable_actions.cpp
+++ contracts/auditor.bos/src/external_observable_actions.cpp
@@ -4,22 +4,34 @@ void auditorbos::transfer( name from,
                            const std::string& memo ) {
 
     if (to == _self) {
-        const auto & reg_candidate = _candidates.find(from.value);
+        const auto & candidate_itr = _candidates.find(from.value);
         const time_point_sec unstaking_period = current_time_point() + time_point_sec(configs().lockup_release_time_delay);
 
+        bool is_active = false;
+
         // Modify existing registered candidate with additional locked_tokens
-        if (reg_candidate != _candidates.end()) {
-            _candidates.modify(reg_candidate, eosio::same_payer, [&](auto & row) {
+        if (candidate_itr != _candidates.end()) {
+
+            // Auto nominate candidate if locked_tockens is above minimum threshold
+            if (candidate_itr->locked_tokens + quantity >= configs().lockupasset) is_active = true;
+
+            // Modify locked_tokens amount & set new unstaking period
+            _candidates.modify(candidate_itr, eosio::same_payer, [&](auto & row) {
                 row.locked_tokens += quantity;
                 row.unstaking_end_time_stamp = unstaking_period;
+                row.is_active = is_active;
             });
         // New candidate, register them and add locked_tokens
         } else {
+            // Auto nominate candidate if locked_tockens is above minimum threshold
+            if (candidate_itr->locked_tokens >= configs().lockupasset) is_active = true;
+
+            // Add candidate to table
             _candidates.emplace(_self, [&](auto & row) {
                 row.candidate_name = from;
                 row.locked_tokens = quantity;
                 row.total_votes = 0;
-                row.is_active = 0;
+                row.is_active = is_active;
                 row.unstaking_end_time_stamp = unstaking_period;
             });
         }
