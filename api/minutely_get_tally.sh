0ff9fd3756ce4e8f8932b19b7429f669d17092ad
pursonchen <185297167@qq.com>
2019-08-09 21:20:21 +0800

    fix shell

--- api/minutely_get_tally.sh
+++ api/minutely_get_tally.sh
@@ -1,14 +1,14 @@
 # 定时下载任务
 
-rm /root/referendum/api/proposal_tally.json
+
 tmp=`wget https://s3.amazonaws.com/bos.referendum/referendum/tallies/latest.json`
-find /root/referendum/api/latest.json -exec mv {} /root/referendum/api/proposal_tally.json \;
+find latest.json -exec mv {} /root/referendum/api/proposal_tally.json \;
 
 
-rm /root/referendum/api/vote_total_tally.json
+
 tmp=`wget https://s3.amazonaws.com/bos.referendum/referendum/summaries/latest.json`
-find /root/referendum/api/latest.json -exec mv {} /root/referendum/api/vote_total_tally.json \;
+find latest.json -exec mv {} /root/referendum/api/vote_total_tally.json \;
 
-rm /root/referendum/api/auditor_tally.json
+
 tmp=`wget https://s3.amazonaws.com/bos.referendum/referendum/auditor.tallies/latest.json`
-find /root/referendum/api/latest.json -exec mv {} /root/referendum/api/auditor_tally.json \;
+find latest.json -exec mv {} /root/referendum/api/auditor_tally.json \;
