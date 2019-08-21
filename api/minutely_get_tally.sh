9a722c66d6d06beff170f2530525a09aaca8fe32
pursonchen <185297167@qq.com>
2019-08-21 11:51:18 +0800

    udpate cron url

--- api/minutely_get_tally.sh
+++ api/minutely_get_tally.sh
@@ -1,12 +1,12 @@
 # 定时下载任务
 
 
-tmp=`wget https://api.boswps.com/eosio.forum/proposal/latest.json`
+tmp=`wget https://s3.amazonaws.com/bos.referendum/referendum/forum.tallies/latest.json`
 find latest.json -exec mv {} /root/referendum/api/proposal_tally.json \;
 
 
 
-tmp=`wget https://s3.amazonaws.com/bos.referendum/referendum/summaries/latest.json`
+tmp=`wget https://api.boswps.com/eosio/stats/latest.json`
 find latest.json -exec mv {} /root/referendum/api/vote_total_tally.json \;
 
 
