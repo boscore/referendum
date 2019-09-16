40cf5ccd31a5e3110115f78183cea5f5cb135629
pursonchen <185297167@qq.com>
2019-09-16 14:57:58 +0800

    fix get Json

--- api/index.py
+++ api/index.py
@@ -78,106 +78,106 @@ def jsoninfo():
 		proposals = json.loads(f.read())
 	# get Voted Total
 	#iterater the current proposals
-	for proposal in proposals:
-		proposal_item = proposals[proposal]
-		vote_key = proposal_item['stats']['votes']
-		stake_key = proposal_item['stats']['staked']
-		staked_total = float(int(proposal_item['stats']['staked']['total'])) if 'total' in stake_key else 0
-		vote_total = proposal_item['stats']['votes']['total'] if  'total' in vote_key else 0
-		vote_yes = proposal_item['stats']['votes']['1'] if  '1' in vote_key else 0
-		vote_no = proposal_item['stats']['votes']['0'] if '0' in vote_key else 0
-		_meet_conditions_days =  0
-		_approved_by_vote =  0
-		_approved_by_vote_date =  None
-		_approved_by_BET =  0
-		_reviewed_by_BET_date =  None
-		_approved_by_BPs =  0
-		_approved_by_BPs_date =  None
-		_review = 0
-		_review_date = None
-		_finish =  0
-		_finish_date =  None
-		# check if the proposal exists
-		try:
-			propos = Proposal.select().where(Proposal.name == proposal).count()
-			logger.info("result"+str(propos))				
-			# the proposal exists
-			if propos > 0:
-				# check if the proposal is passed
-				if proposal_base_condition_ckeck(BP_TOTAL_VOTES, staked_total, vote_yes, vote_no):
-					propos = Proposal.get(Proposal.name == proposal)
-					meet = propos.meet_conditions_days
-					# abv = propos.approved_by_vote 
-					abvd = propos.approved_by_vote_date
-					_approved_by_vote = 1 if int(meet) >= PROPOSAL_MEET_DAYS else 0
-					_approved_by_vote_date = datetime.now() if abvd == None else abvd
-					# if meet >= 0 and abv != False and abvd != None:
-					nrow = (Proposal.update(
-						json_info = proposal_item
-						, vote_total = vote_total
-						, staked_total = staked_total
-						, vote_bp_total = BP_TOTAL_VOTES
-						, vote_yes = vote_yes
-						, vote_no = vote_no
-						, meet_conditions_days = int(meet) + 1 if int(meet) < PROPOSAL_MEET_DAYS + 1 else int(meet)						, approved_by_vote = _approved_by_vote						, approved_by_vote_date = _approved_by_vote_date
-						).where(Proposal.name == proposal).execute())
+		for proposal in proposals:
+			proposal_item = proposals[proposal]
+			vote_key = proposal_item['stats']['votes']
+			stake_key = proposal_item['stats']['staked']
+			staked_total = float(int(proposal_item['stats']['staked']['total'])) if 'total' in stake_key else 0
+			vote_total = proposal_item['stats']['votes']['total'] if  'total' in vote_key else 0
+			vote_yes = proposal_item['stats']['votes']['1'] if  '1' in vote_key else 0
+			vote_no = proposal_item['stats']['votes']['0'] if '0' in vote_key else 0
+			_meet_conditions_days =  0
+			_approved_by_vote =  0
+			_approved_by_vote_date =  None
+			_approved_by_BET =  0
+			_reviewed_by_BET_date =  None
+			_approved_by_BPs =  0
+			_approved_by_BPs_date =  None
+			_review = 0
+			_review_date = None
+			_finish =  0
+			_finish_date =  None
+			# check if the proposal exists
+			try:
+				propos = Proposal.select().where(Proposal.name == proposal).count()
+				logger.info("result"+str(propos))				
+				# the proposal exists
+				if propos > 0:
+					# check if the proposal is passed
+					if proposal_base_condition_ckeck(BP_TOTAL_VOTES, staked_total, vote_yes, vote_no):
+						propos = Proposal.get(Proposal.name == proposal)
+						meet = propos.meet_conditions_days
+						# abv = propos.approved_by_vote 
+						abvd = propos.approved_by_vote_date
+						_approved_by_vote = 1 if int(meet) >= PROPOSAL_MEET_DAYS else 0
+						_approved_by_vote_date = datetime.now() if abvd == None else abvd
+						# if meet >= 0 and abv != False and abvd != None:
+						nrow = (Proposal.update(
+							json_info = proposal_item
+							, vote_total = vote_total
+							, staked_total = staked_total
+							, vote_bp_total = BP_TOTAL_VOTES
+							, vote_yes = vote_yes
+							, vote_no = vote_no
+							, meet_conditions_days = int(meet) + 1 if int(meet) < PROPOSAL_MEET_DAYS + 1 else int(meet)						, approved_by_vote = _approved_by_vote						, approved_by_vote_date = _approved_by_vote_date
+							).where(Proposal.name == proposal).execute())
+					else:
+						nrow = (Proposal.update(
+							json_info = proposal_item
+							, vote_total = vote_total
+							, staked_total = staked_total
+							, vote_bp_total = BP_TOTAL_VOTES
+							, vote_yes = vote_yes
+							, vote_no = vote_no
+							, meet_conditions_days = 0
+							, approved_by_vote = 0
+							, approved_by_vote_date = None
+							, approved_by_BET = 0
+							, reviewed_by_BET_date = None
+							, approved_by_BPs = 0
+							, approved_by_BPs_date = None
+							, review = 0
+							, review_date = None
+							, finish = 0
+							, finish_date = None
+							).where(Proposal.name == proposal).execute())
 				else:
-					nrow = (Proposal.update(
-						json_info = proposal_item
-						, vote_total = vote_total
-						, staked_total = staked_total
-						, vote_bp_total = BP_TOTAL_VOTES
-						, vote_yes = vote_yes
-						, vote_no = vote_no
-						, meet_conditions_days = 0
-						, approved_by_vote = 0
-						, approved_by_vote_date = None
-						, approved_by_BET = 0
-						, reviewed_by_BET_date = None
-						, approved_by_BPs = 0
-						, approved_by_BPs_date = None
-						, review = 0
-						, review_date = None
-						, finish = 0
-						, finish_date = None
-						).where(Proposal.name == proposal).execute())
-			else:
-				if proposal_base_condition_ckeck(BP_TOTAL_VOTES, staked_total, vote_yes, vote_no):
-					_approved_by_vote = 1
-					_approved_by_vote_date = datetime.now()
-				else:
-					pass
-				new_proposal = Proposal(name = proposal
-				, json_info = proposal_item
-				, vote_total = vote_total
-				, staked_total = staked_total
-				, vote_bp_total = BP_TOTAL_VOTES
-				, vote_yes = vote_yes
-				, vote_no = vote_no
-				, meet_conditions_days = _meet_conditions_days
-				, approved_by_vote = _approved_by_vote
-				, approved_by_vote_date = _approved_by_vote_date
-				, approved_by_BET = _approved_by_BET
-				, reviewed_by_BET_date = _reviewed_by_BET_date
-				, approved_by_BPs = _approved_by_BPs
-				, approved_by_BPs_date = _approved_by_BPs_date
-				, review = 0
-				, review_date = None
-				, finish = _finish
-				, finish_date = _finish_date)
-				new_proposal.save()  
-		except Exception as err:
-			logger.error(err)
-			alarm(err)
-		logger.info("proposal name: " + proposal)
-		logger.info("vote total: " + str(vote_total))
-		logger.info("staked total: " + str(staked_total))
-		logger.info("vote yes: " + str(vote_yes))
-		logger.info("vote no: " + str(vote_no))
-		logger.info("___________________")
-	resp = flask.Response(proposals)
-	resp.headers['Access-Control-Allow-Origin'] = '*'
-	return resp
+					if proposal_base_condition_ckeck(BP_TOTAL_VOTES, staked_total, vote_yes, vote_no):
+						_approved_by_vote = 1
+						_approved_by_vote_date = datetime.now()
+					else:
+						pass
+					new_proposal = Proposal(name = proposal
+					, json_info = proposal_item
+					, vote_total = vote_total
+					, staked_total = staked_total
+					, vote_bp_total = BP_TOTAL_VOTES
+					, vote_yes = vote_yes
+					, vote_no = vote_no
+					, meet_conditions_days = _meet_conditions_days
+					, approved_by_vote = _approved_by_vote
+					, approved_by_vote_date = _approved_by_vote_date
+					, approved_by_BET = _approved_by_BET
+					, reviewed_by_BET_date = _reviewed_by_BET_date
+					, approved_by_BPs = _approved_by_BPs
+					, approved_by_BPs_date = _approved_by_BPs_date
+					, review = 0
+					, review_date = None
+					, finish = _finish
+					, finish_date = _finish_date)
+					new_proposal.save()  
+			except Exception as err:
+				logger.error(err)
+				alarm(err)
+			logger.info("proposal name: " + proposal)
+			logger.info("vote total: " + str(vote_total))
+			logger.info("staked total: " + str(staked_total))
+			logger.info("vote yes: " + str(vote_yes))
+			logger.info("vote no: " + str(vote_no))
+			logger.info("___________________")
+		resp = flask.Response(proposals)
+		resp.headers['Access-Control-Allow-Origin'] = '*'
+		return resp
 	# except Exception as err:
 	# 		return err
 
